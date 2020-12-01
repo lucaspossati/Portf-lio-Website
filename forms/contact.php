35b431e747e799757badd2fc57cfa9d9f58a8206
Lucas Davi Possati de Souza <lucasdavipossati@hotmail.com>
2020-12-01 01:31:36 -0300

    Melhorias no portfolio

--- forms/contact.php
+++ forms/contact.php
@@ -9,10 +9,10 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'contact@example.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  if( file_exists($php_email_form = 'php-email.php' )) {
     include( $php_email_form );
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    die( 'Não foi possível enviar o e-mail pois essa é a versão que está no GitHub!');
   }
 
   $contact = new PHP_Email_Form;
@@ -23,15 +23,13 @@
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
     'host' => 'example.com',
     'username' => 'example',
     'password' => 'pass',
     'port' => '587'
   );
-  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
